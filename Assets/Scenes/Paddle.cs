b19dd9945e3412cfe1c2e3d677140bf6ba2cad63
Juyeong Maing <mjy9088@naver.com>
2024-05-01 21:35:49 +0900

    add paddle movement

--- Assets/Scenes/Paddle.cs
+++ Assets/Scenes/Paddle.cs
@@ -9,11 +9,39 @@ public class Paddle : MonoBehaviour
         extents = 4f,
         speed = 10f;
 
+    [SerializeField]
+    bool isAI;
+
     public void Move(float target, float arenaExtents)
     {
         Vector3 p = transform.localPosition;
+        p.x = isAI ? AdjustByAI(p.x, target) : AdjustByPlayer(p.x);
         float limit = arenaExtents - extents;
         p.x = Mathf.Clamp(p.x, -limit, limit);
         transform.localPosition = p;
+    }
+
+    float AdjustByAI(float x, float target)
+    {
+        if (x < target)
+        {
+            return Mathf.Min(x + speed * Time.deltaTime, target);
+        }
+        return Mathf.Max(x - speed * Time.deltaTime, target);
+    }
+
+    float AdjustByPlayer(float x)
+    {
+        bool goRight = Input.GetKey(KeyCode.RightArrow);
+        bool goLeft = Input.GetKey(KeyCode.LeftArrow);
+        if (goRight && !goLeft)
+        {
+            return x + speed * Time.deltaTime;
+        }
+        else if (goLeft && !goRight)
+        {
+            return x - speed * Time.deltaTime;
+        }
+        return x;
     }
 }
