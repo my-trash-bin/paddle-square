074f4738f799329ddd4ae6ba6429d7182d4ab89f
Juyeong Maing <mjy9088@naver.com>
2024-05-05 03:27:32 +0900

    remove custom weird movement

--- Assets/Scenes/Ball.cs
+++ Assets/Scenes/Ball.cs
@@ -8,16 +8,7 @@ public class Ball : MonoBehaviour
     float
         constantXSpeed = 8f,
         constantYSpeed = 10f,
-        randomXSpeed = 2f,
-        randomYSpeed = 2f,
         extents = 0.5f;
-
-    readonly float speed;
-
-    Ball()
-    {
-        this.speed = System.MathF.Sqrt(constantXSpeed * constantXSpeed + constantYSpeed * constantYSpeed);
-    }
 
     public float Extents => extents;
 
@@ -30,7 +21,7 @@ public class Ball : MonoBehaviour
     public void UpdateVisualization() =>
         transform.localPosition = new Vector3(position.x, 0f, position.y);
 
-    public void Move() => position += velocity / velocity.magnitude * speed * Time.deltaTime;
+    public void Move() => position += velocity * Time.deltaTime;
 
     public void StartNewGame()
     {
@@ -39,26 +30,15 @@ public class Ball : MonoBehaviour
         velocity = new Vector2(constantXSpeed, -constantYSpeed);
     }
 
-    private float Random()
-    {
-        return (float)random.NextDouble() * 2.0f - 1.0f;
-    }
-
     public void BounceX(float boundary)
     {
         position.x = 2f * boundary - position.x;
-        float signX = velocity.x > 0 ? 1 : -1;
-        float signY = velocity.y > 0 ? 1 : -1;
-        velocity.x = -signX * constantXSpeed + Random() * randomXSpeed;
-        velocity.y = signY * constantYSpeed + Random() * randomYSpeed;
+        velocity.x *= -1;
     }
 
     public void BounceY(float boundary)
     {
         position.y = 2f * boundary - position.y;
-        float signX = velocity.x > 0 ? 1 : -1;
-        float signY = velocity.y > 0 ? 1 : -1;
-        velocity.y = -signY * constantYSpeed + Random() * randomYSpeed;
-        velocity.x = signX * constantXSpeed + Random() * randomXSpeed;
+        velocity.y *= -1;
     }
 }
